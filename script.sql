e1f38f5849d766602db9de88c1eb4a874479618d
arthur <arthur.queiroz08@aluno.ifce.edu.br>
2024-02-06 20:51:53 -0300

    fix script sql

--- script.sql
+++ script.sql
@@ -4,24 +4,24 @@ CREATE TABLE clientes (
   saldo INTEGER NOT NULL
 );
 
-CREATE TABLE transacao (
+CREATE TABLE transacoes (
   id SERIAL PRIMARY KEY,
   cliente_id INTEGER NOT NULL,
   tipo VARCHAR(1) NOT NULL,
   valor INTEGER NOT NULL,
   descricao VARCHAR(10) NOT NULL,
-  realizado TIMESTAMP NOT NULL DEFALT NOW()
+  realizado TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_extrato ON transacoes (id DESC);
 
 DO $$
 BEGIN
-  INSERT INTO clientes (nome, limite)
+  INSERT INTO clientes (limite, saldo)
   VALUES
-    ('o barato sai caro', 1000 * 100),
-    ('zan corp ltda', 800 * 100),
-    ('les cruders', 10000 * 100),
-    ('padaria joia de cocaia', 100000 * 100),
-    ('kid mais', 5000 * 100);
+    (1000 * 100, 0),
+    (800 * 100, 0),
+    (10000 * 100, 0),
+    (100000 * 100, 0),
+    (5000 * 100, 0);
 END; $$
